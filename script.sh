edd10b086ed9493931ccabf8b26aa3c672f2ee1b
wishiknew <73454467+wishiknew@users.noreply.github.com>
2024-05-17 20:02:46 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
-python3 -m venv /Users/vishnutej/code/wishiknew/d11n/camp/pdf-to-doc/venv
-source /Users/vishnutej/code/wishiknew/d11n/camp/pdf-to-doc/venv/bin/activate
+python3 -m venv /path-to-folder/pdf-to-doc/venv
+source /path-to-folder/pdf-to-doc/venv/bin/activate
 python3 -m pip install -r requirements.txt
